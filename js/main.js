438fe8361366a29488b9adf43218cddec9b55bd3
Diana Dzakah <dianadzakah12gmail.com>
2024-02-12 05:55:05 -0800

    working on appy Navbar

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+console.log('we are beta!!!!');
+var firstname='Diana';
+var age=16;
+let lastname='Dzakah'
+const boilingWaterTemperature=100;
+console.log(firstname)
+console.log(lastname)
+console.log(age)
+let bottle = {
+
+}
+//condition statements (if/else)
+if(age>=18) {
+    console.log('you are eligible');
+}else{
+    console.log("wait till 18")
+}
+
+//loops(for)
+for (let i=1; i < 8;i++){console.log('i am at index:' , i);
+
+}
+//function-Define
+function iselegible
